fbf14c1cc79a47cc53444d9d49b0f9ae03cd1baf
Dalal Alsabah <dalalalsabah@Dalals-MacBook-Pro.local>
2022-09-29 00:02:10 +0300

    fixing errors vendor class

--- main.py
+++ main.py
@@ -1,38 +1,69 @@
 class Wallet:
-    def __init__(self, money):
-        pass
+    def __init__(self,money):
+        self.money= money
 
-    def credit(amount):
-        pass
+    def credit(self,amount):
+        self.money += amount
+        print (f"you have {self.money} remaing in wallet")
 
-    def debit(amount):
-        pass
+    def debit(self,amount):
+        self.money -= amount
+        f"you have {self.money} remaining in wallet"
+
 
 
 wallet = Wallet(6)
-wallet = Wallet()  # This should default money inside the wallet to 0
+wallet = Wallet(0)  # This should default money inside the wallet to 0
 print(wallet)
 
 
 class Person:
-    # Implement the code here
-    pass
+    def __init__(self, name, location, money):
+        self.name= name
+        self.location= location
+        self.wallet= Wallet(money)
+
+    def moveto(self, point):
+        self.location = point
+        print(f"you are located in {self.location}")
+
+
+
+    # Implement the code here pass
 
 
 person = Person("Moh", 5, 50)
+person.moveto(10)
+
+class Vendor(Person):
+    def __init__(self, name, location, money,range, price):
+        super().__init__(name, location, money) 
+        self.range= range 
+        self.price= price
+    
+    def sellTo(self, customer, number_of_icecreams):
+        vendor= Vendor(Person)
+        vendor.moveto(customer) 
+        Vendor.wallet += (number_of_icecreams* self.price)
+        print(f"you bought {number_of_icecreams} icecream")
+            
+        
 
 
-class Vendor:
+    
+
+
+
     # implement Vendor!
     pass
 
 
-vendor = Vendor("Abdallah", 3, 6)
-
+vendor = Vendor("Abdallah", 3, 6,5,1)
+vendor.sellTo(5,10)
 
 class Customer:
     # implement Customer!
     pass
 
 
-customer = Customer("Abdallah", 3, 6)
+#customer = Customer("Abdallah", 3, 6)
